86558dedfc3fa194c0ff5a712f9b06d76e5ead93
Monashko Oleh <msi1050ti@gmail.com>
2023-07-04 21:16:18 +0300

    test2 commit IDEA

--- src/main/java/ua/hillel/monashko/lessons/lesson3/Main.java
+++ src/main/java/ua/hillel/monashko/lessons/lesson3/Main.java
@@ -5,6 +5,10 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello GIT");
+        System.out.println("Hello GIT");
+        System.out.println("Hello GIT");
+
+
 
     }
 
