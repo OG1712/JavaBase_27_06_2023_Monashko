8cdb0515bca7acd6a8960c4fd97556fe7fa803cf
Monashko Oleh <msi1050ti@gmail.com>
2023-07-15 15:11:22 +0300

    lesson 4

--- src/main/java/ua/hillel/monashko/lessons/lesson2/Main.java
+++ src/main/java/ua/hillel/monashko/lessons/lesson2/Main.java
@@ -3,28 +3,99 @@ package ua.hillel.monashko.lessons.lesson2;
 public class Main {
     public static void main(String[] args) {
 
-        //  int age;
-        //   age = 234;
-        //    short shortVariable = 32000;
-        //     int intVariable = 231212321;
-        //      long longVariable = 222323123213L;
+        ////        byte age;
+////        age = 35;
+//
+//        byte age = 35;
+//        short shortVariable = 32000;
+//        int intVariable = 2147000000;
+//        long longVariable = 21470000000000L;
+//
+//        System.out.println(age);
+//
+//        age = 22;
+//
+//        System.out.println(age);
+//
+//        age = 29;
+//
+//
+//
+////        float floatVariable = 1.123456789123456789F;
+////        double doubleVariable = 1.123456789123456789;
+//////        System.out.println(floatVariable);
+//////        System.out.println(doubleVariable);
+////
+////
+////
+////        char charA1 = 'A';
+////        char charA2 = 65;
+////        char charA3 = '\u0041';
+////
+////        System.out.println(charA1);
+////        System.out.println(charA2);
+////        System.out.println(charA3);
+////
+////
+////        boolean isOK = true;
+////        boolean isOK2 = false;
+////
+////        System.out.println(isOK);
+////        System.out.println(isOK2);
+////
+//        final double PI_VALUE;
+//        PI_VALUE = 3.14;
+//
+//        System.out.println(PI_VALUE);
 
-        // System.out.println(age);
 
-        // float floatVariable = 1.123456789123456789F;
-        //  double doubleVariable = 1.123456789123456789;
-        //   System.out.println(floatVariable);
+//        byte byteVariable = 55;
+//        double doubleTest = byteVariable;
+//
+//        System.out.println(byteVariable);
+//        System.out.println(doubleTest);
 
-        // System.out.println(doubleVariable);
 
-        //   char chA = 'A';
-        // int intA = chA;
+//        char chA = 'A';
+//        int intA = chA;
+//
+//        System.out.println(chA);
+//        System.out.println(intA);
 
-        //      System.out.println( chA );
-        //    System.out.println( intA);
 
-        String str = "Hello Word " + 555 + " Java Best";
+//        int intAge = 351231232;
+//        byte byteAge = (byte) intAge;
+//
+//        System.out.println(intAge);
+//        System.out.println(byteAge);
+//
+//
+//        int intAge2 = byteAge;
+//        System.out.println(intAge2);
+
+
+//        double doubleVariable = 13.999999;
+//        int intVariable = (int) doubleVariable;
+//
+//        System.out.println(intVariable);
+
+        String str1 = "Hello";
+        String str2 = "World";
+        String str3 = "Java";
+        String str4 = "Best";
+
+        int number = 555;
+
+        String str = str1 + " " + str2 + " " + number + " " + str3 + " " + str4 + 1 + 1;
         System.out.println(str);
+
+
+//        double longitude = 0.116773;
+//        double latitude = 51.510357;
+//        char specSymbol = '\u00B0';
+//        char specSymbol2 = '\'';
+//        System.out.println("Longitude: - " + longitude + " Latitude: " + latitude);
+//        System.out.println("\"DMS Lat\": 51" + specSymbol + " 30" + specSymbol2 + " 37.2852'' N   DMS Long: 0° 7' 0.3828'' W");
 
     }
 }
