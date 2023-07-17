0e5335b6eaa2bc45d04817e125ce2e37912c7616
Monashko Oleh <msi1050ti@gmail.com>
2023-07-17 22:05:49 +0300

    Homework 8

---
+++ src/main/java/ua/hillel/monashko/homeworks/homework8/Main.java
@@ -0,0 +1,39 @@
+package ua.hillel.monashko.homeworks.homework8;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int counter = 0;
+        int i = 0;
+        for (; i <= 100; i++) {
+
+
+            if (i / 10 == 4 || i / 10 == 9 || i % 10 == 4 || i % 10 == 9 || i / 100 == 4 || i / 100 == 9 || i % 100 == 4 || i % 100 == 9) {
+                continue;
+            }
+            System.out.println("Shuttle №- " + i);
+            counter++;
+
+        }
+        i--;
+
+
+        while (counter < 100) {
+            i++;
+            counter++;
+            if (i / 10 == 4 || i / 10 == 9 || i % 10 == 4 || i % 10 == 9 || i / 100 == 4 || i / 100 == 9 || i % 100 == 4 || i % 100 == 9) {
+                continue;
+
+            }
+            System.out.println("Shuttle №- " + i);
+
+
+        }
+        System.out.println(counter);
+    }
+}
+
+
+
+
+
