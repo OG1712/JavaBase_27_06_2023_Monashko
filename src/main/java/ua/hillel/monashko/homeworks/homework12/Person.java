dedd7188467ea311bafa029d378613d2295e3a42
Monashko Oleh <msi1050ti@gmail.com>
2023-08-10 19:45:35 +0300

    Homework 12

---
+++ src/main/java/ua/hillel/monashko/homeworks/homework12/Person.java
@@ -0,0 +1,37 @@
+package ua.hillel.monashko.homeworks.homework12;
+
+public class Person {
+    public static void main(String[] args) {
+        personInfo1();
+        personInfo2();
+        personInfo3();
+    }
+
+    static void personInfo1() {
+        String strName1 = "Will";
+        String strSecondName1 = "Smith";
+        String strCity1 = "New York";
+        String strPhone1 = "2936729462846";
+        System.out.println("Зателефонувати громадянину" + " " + strName1 + " " + strSecondName1 + " " +
+                "із міста" + " " + strCity1 + " " + "можна за номером" + " " + strPhone1);
+
+    }
+
+    static void personInfo2() {
+        String strName2 = "Jackie";
+        String strSecondName2 = "Chan";
+        String strCity2 = "Shanghai";
+        String strPhone2 = "12312412412";
+        System.out.println("Зателефонувати громадянину" + " " + strName2 + " " + strSecondName2 + " " +
+                "із міста" + " " + strCity2 + " " + "можна за номером" + " " + strPhone2);
+    }
+    static void personInfo3() {
+        String strName3 = "Sherlock";
+        String strSecondName3 = "Holmes";
+        String strCity3 = "London";
+        String strPhone3 = "37742123513";
+        System.out.println("Зателефонувати громадянину" + " " + strName3 + " " + strSecondName3 + " " +
+                "із міста" + " " + strCity3 + " " + "можна за номером" + " " + strPhone3);
+    }
+
+}
